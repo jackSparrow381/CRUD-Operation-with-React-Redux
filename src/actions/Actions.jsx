3feba4d5b8c1d47099320554425668913624d103
jackSparrow381 <developerasad381@gmail.com>
2022-10-01 12:52:28 +0500

    Modified Actions

--- src/actions/Actions.jsx
+++ src/actions/Actions.jsx
@@ -1,22 +1,22 @@
-const ADD_TODO = () => {
+export const ADD_TODO = () => {
   return {
     type: "ADD_TODO",
   };
 };
 
-const REMOVE_TODO = () => {
+export const REMOVE_TODO = () => {
   return {
     type: "REMOVE_TODO",
   };
 };
 
-const EDIT_TODO = () => {
+export const EDIT_TODO = () => {
   return {
     type: "EDIT_TODO",
   };
 };
 
-const UPDATE_TODO = () => {
+export const UPDATE_TODO = () => {
   return {
     type: "UPDATE_TODO",
   };
